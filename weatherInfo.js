ed5748196491edc82d389c6e6d1bee51824954e9
happy gikundiro <gikundirohappy@gmail.com>
2023-11-07 23:58:09 +0200

    program to convert fahrenheit to celcius

---
+++ weatherInfo.js
@@ -0,0 +1,9 @@
+function weatherInfo(temp) {
+  var c = convertToCelsius(temp);
+  if (c >= 0) return c + "is above freezing temperature";
+  else return c + "is above freezing temperature";
+}
+function convertToCelsius(temperature) {
+  var celsius = (temperature - 32) * (5 / 9);
+  return celsius;
+}
