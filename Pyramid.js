8a8c150c822f860a63af8f65fd341be0a318981f
happy gikundiro <gikundirohappy@gmail.com>
2023-11-27 18:14:14 +0200

    a program to display pyramid pattern

---
+++ Pyramid.js
@@ -0,0 +1,26 @@
+for (i = 0; i < 5; i++) {
+  var str = "";
+  for (j = 1; j <= i; j++) {
+    str += "*";
+  }
+  console.log(str);
+}
+console.log("-----------------");
+
+for (i = 0; i < 5; i++) {
+  var str = "";
+  for (j = 1; j <= i; j++) {
+    str += i;
+  }
+  console.log(str);
+}
+console.log("-----------------");
+
+for (i = 4; i > 0; i--) {
+  var str = "";
+  for (j = 1; j <= i; j++) {
+    str += j;
+  }
+  console.log(str);
+}
+console.log("-----------------");
