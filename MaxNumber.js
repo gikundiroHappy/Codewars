3cc0f0d1a2eb745457ff931e5463d692bdf16938
happy gikundiro <gikundirohappy@gmail.com>
2023-11-27 17:21:40 +0200

    created a function to display the maximum number in the array

---
+++ MaxNumber.js
@@ -0,0 +1,15 @@
+// Define an array
+
+function MaxNumber() {
+  const numbers = [1, 20, 10, 7, 2, 9];
+  let maxNumber = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNumber) {
+      maxNumber = numbers[i];
+    }
+  }
+  console.log(maxNumber);
+}
+
+MaxNumber();
