1c45ff6a003b6f5833da72b463fbb3584cb5b65b
happy gikundiro <gikundirohappy@gmail.com>
2024-02-06 10:59:20 +0200

    added a task folder

--- Pyramid.js
+++ Pyramid.js
@@ -24,3 +24,18 @@ for (i = 4; i > 0; i--) {
   console.log(str);
 }
 console.log("-----------------");
+
+let n = 5;
+let string = "";
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= n - i; j++) {
+    string += " ";
+  }
+
+  for (let k = 0; k < 2 * i - 1; k++) {
+    string += "*";
+  }
+  string += "\n";
+}
+console.log(string);
--- removeVowel.js
+++ removeVowel.js
@@ -1,4 +1,18 @@
-function removeVowel(str) {
-  return str.replace(/[aeiouAEIOU]/g, "");
+// function removeVowel(str) {
+//   return str.replace(/[aeiouAEIOU]/g, "");
+// }
+// console.log(removeVowel("Eddyyuiure"));
+
+let vowels = ["a", "i", "u", "o", "e"];
+
+function removeVowel(word) {
+  let result = word
+    .split("")
+    .filter((char) => !vowels.includes(char))
+    .join("");
+  return result;
 }
-console.log(removeVowel("Eddyyuiure"));
+
+let inputWord = "example";
+let result = removeVowel(inputWord);
+console.log(result);
